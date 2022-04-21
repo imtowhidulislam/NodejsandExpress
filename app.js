c3c4cab5e5eb71c1abfa001629029bfcf86c4461
towhid <niamaaktar@gmail.com>
2022-04-22 01:37:10 +0600

    all files are updated

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const fs = require("fs");
-const fileReadStream = fs.createReadStream("./content/textfile1.txt", "utf8");
-const fileWriteStream = fs.createWriteStream("./content/output.txt", "utf8");
+const express = require("express");
 
-// ? Option one
-/* fileReadStream.on("data", (text) => {
-  console.log(text);
-  fileWriteStream.write(text);
+const app = express();
+const { person } = require("./data");
+
+app.get("/api/person/:personId", (req, res) => {
+  const { personId } = req.params;
+  console.log(personId);
+  const singlePerson = person.find((onePerson) => onePerson.id === +personId);
+  if (!singlePerson) {
+    return res.status(404).send(`content doesn't found`);
+  }
+  res.json(singlePerson);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  const { search, limit } = req.query;
+  let filterData = [...person];
+
+  if (search) {
+    filterData = filterData.filter((person) => person.name.startsWith(search));
+  }
+
+  if (limit) {
+    filterData = filterData.slice(0, Number(limit));
+  }
+
+  if (filterData.length < 1) {
+    return res.status(200).json({ success: true, data: [] });
+  }
+
+  return res.status(200).json(filterData);
+});
+
+/* app.get("/contact", (req, res) => {
+  res.send("This is contact page");
 }); */
 
-// ? Option two:::
-fileReadStream.pipe(fileWriteStream);
-console.log("i am first");
+/* app.get("/profile/:profileId", (req, res) => {
+  res.send("You requested the profile with the Id of :" + req.params.profileId);
+}); */
+
+// ? Absolute path for the file..
+app.get("/about", (req, res) => {
+  res.sendfile(__dirname + "/about.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ data.js
@@ -0,0 +1,36 @@
+const person = [
+  {
+    id: 1,
+    name: "towhid",
+    age: 23,
+    country: "Bangladesh",
+    state: "Dhaka",
+    designation: "web developer",
+  },
+  {
+    id: 2,
+    name: "Niama",
+    age: 17,
+    country: "Bangladesh",
+    state: "Dhaka",
+    designation: "Student",
+  },
+  {
+    id: 3,
+    name: "Asif",
+    age: 23,
+    country: "Bangladesh",
+    state: "Comilla",
+    designation: "apps developer",
+  },
+  {
+    id: 4,
+    name: "Alamin",
+    age: 22,
+    country: "Bangladesh",
+    state: "Comilla",
+    designation: "web developer",
+  },
+];
+
+module.exports = { person };
